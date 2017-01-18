129b11dd6896a4248e5ec903345c8ab9347e0247
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-01-18 13:00:22 +0100

    First Commit

---
+++ Web Forum.data/Models/Subforum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web_Forum.data.Models;
+
+namespace Web_Forum.data
+{
+    public class Subforum
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Topic> Topics { get; set; }
+    }
+}
---
+++ Web Forum.data/Models/Thread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Forum.data.Models
+{
+    public class Thread
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
