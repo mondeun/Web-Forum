05ab348e209e72f442148b10ad0b256727a45fab
asvimalo <asvimalo@gmail.com>
2017-01-18 15:15:37 +0100

    testing....

--- Web Forum/Controllers/HomeController.cs
+++ Web Forum/Controllers/HomeController.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Web_Forum.data.Interfaces;
+using Web_Forum.data.Repositories;
 
 
 namespace Web_Forum.Controllers
 {
+   
+
     public class HomeController : Controller
     {
-        
+        public IRepository repo = new DummyRepository();
         public ActionResult Index()
         {
-            return View();
+            //var threads = repo.GetThreads();
+            return View(/*threads*/);
         }
-        public ActionResult getSubforums()
-        {
-            return View();
-        }
+        
      
 
         
