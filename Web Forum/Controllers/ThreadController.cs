c58511933b23ff384ec9ff4c9b1d39452bc58edd
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-01-19 14:21:08 +0100

    Added testing

--- Web Forum/Controllers/ThreadController.cs
+++ Web Forum/Controllers/ThreadController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Web_Forum.data.Interfaces;
 using Web_Forum.data.Models;
 using Web_Forum.data.Repositories;
+using Web_Forum.Helpers;
 using Web_Forum.Models;
 
 namespace Web_Forum.Controllers
@@ -30,12 +31,12 @@ namespace Web_Forum.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddPost(Guid threadId,PostViewModel post)
+        public ActionResult AddPost(PostViewModel post)
         {
             //TODO Add logic here
             if (ModelState.IsValid)
             {
-                repo.AddPost(threadId, post.Transform());
+                repo.AddPost(post.Transform());
             }
             return View("Index");
         }
