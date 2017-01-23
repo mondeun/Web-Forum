dec3c95895dde20ce55792a4dfe84566236a9c85
khroxx <khroxx@DOLPHY>
2017-01-23 12:45:26 +0100

    Removed more unused items

--- Web Forum.data/DTO/ThreadDTO.cs
+++ Web Forum.data/DTO/ThreadDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Web_Forum.data.DTO
+﻿namespace Web_Forum.data.DTO
 {
     public class ThreadDTO
     {
--- Web Forum.data/Interfaces/IRepository.cs
+++ Web Forum.data/Interfaces/IRepository.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Web_Forum.data.DTO;
-using Web_Forum.data.Models;
 
 namespace Web_Forum.data.Interfaces
 {
