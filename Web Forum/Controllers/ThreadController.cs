a2d9dbea1692036d918cd59cf5a0fa408ec00899
khroxx <khroxx@DOLPHY>
2017-01-23 12:42:40 +0100

    Removed unused resources

--- Web Forum/Controllers/ThreadController.cs
+++ Web Forum/Controllers/ThreadController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Web_Forum.data.Interfaces;
-using Web_Forum.data.Models;
 using Web_Forum.data.Repositories;
 using Web_Forum.Helpers;
 using Web_Forum.Models;
-using Web_Forum.Helpers;
 
 namespace Web_Forum.Controllers
 {
