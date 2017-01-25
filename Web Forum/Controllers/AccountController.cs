8e56c5da41d2423ac75f47e2541b13f4ca769a2a
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-01-25 14:40:43 +0100

    Added login functionality

--- Web Forum/Controllers/AccountController.cs
+++ Web Forum/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 using Web_Forum.data.Interfaces;
 using Web_Forum.data.Repositories;
+using Web_Forum.Helpers;
 using Web_Forum.Models;
 
 namespace Web_Forum.Controllers
@@ -29,7 +31,24 @@ namespace Web_Forum.Controllers
             if (!ModelState.IsValid)
                 return PartialView(user);
 
-            var dbUser = new UserViewModel();
+            var dbUser = UserHelper.GetUserViewModelFromDto(repo.GetUserByCredentials(user.Email, user.Password));
+
+            if (dbUser.Email != null && dbUser.Password != null)
+            {
+                var identity = new ClaimsIdentity(new []
+                {
+                    new Claim(ClaimTypes.Name, dbUser.Name),
+                    new Claim(ClaimTypes.Email, dbUser.Email),
+                    new Claim(ClaimTypes.Role, dbUser.IsModerator ? "Mod" : "Guest"), 
+                    new Claim(ClaimTypes.NameIdentifier, dbUser.Id.ToString())
+                }, "UserCookie");
+
+                var ctx = Request.GetOwinContext();
+                var auth = ctx.Authentication;
+                auth.SignIn(identity);
+
+                return RedirectToAction("Index", "Home");
+            }
 
             return PartialView(user);
         }
---
+++ Web Forum/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_Forum.Startup))]
+namespace Web_Forum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "UserCookie",
+                LoginPath = new PathString("/Account/Login")
+            });
+        }
+    }
+}
