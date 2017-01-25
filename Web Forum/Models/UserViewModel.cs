1a77c56e87b73c4d29379dfb3df1f84872e649bd
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-01-25 12:33:52 +0100

    Added more infrastructure for users

---
+++ Web Forum/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_Forum.Models
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool IsModerator { get; set; }
+    }
+}
