cee6e9d96fbb8a6c6411e0f98d701f52fb2d4c8d
vedrana87 <vedrana_andrijevic@hotmail.com>
2017-01-25 08:51:00 +0100

    testing

---
+++ Web Forum/Scripts/modalScript.js
@@ -0,0 +1,18 @@
+﻿
+var modal = document.getElementById('onemodal');
+
+var btn = document.getElementById('ide');
+var span = document.getElementById('close');
+
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
