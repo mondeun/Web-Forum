cbba388658f5eb09d0415188adaaae21eafe55eb
asvimalo <asvimalo@gmail.com>
2017-01-31 10:19:18 +0100

    Fixed some details with delete and refresh

--- Web Forum/Models/ThreadViewModel.cs
+++ Web Forum/Models/ThreadViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace Web_Forum.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Web_Forum.Models
+
+    
 {
     public class ThreadViewModel
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Text { get; set; }
     }
 }
